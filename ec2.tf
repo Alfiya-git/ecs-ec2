3d3b3e82d3a85e05d19f55184188445ba6b6411a
Alfiya-git <137032154+Alfiya-git@users.noreply.github.com>
2023-10-03 13:33:27 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_launch_template" "ecs_lt" {
+ name_prefix   = "ecs-template"
+ image_id      = "ami-0f5ee92e2d63afc18"
+ instance_type = "t3.medium"
+
+# key_name               = "ec2ecsglog"
+ vpc_security_group_ids = [aws_security_group.security_group.id]
+ iam_instance_profile {
+   name = "ecsInstanceRole"
+ }
+
+ block_device_mappings {
+   device_name = "/dev/xvda"
+   ebs {
+     volume_size = 30
+     volume_type = "gp2"
+   }
+ }
+
+ tag_specifications {
+   resource_type = "instance"
+   tags = {
+     Name = "ecs-instance"
+   }
+ }
+
+ #user_data = filebase64("${path.module}/ecs.sh")
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+# variables.tf
+#variable "aws_region" {
+ # type    = string
+  #default = "eu-central-1"
+#}
+
+variable "vpc_cidr" {
+  description = "CIDR block for main"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+#variable "availability_zones" {
+ # type    = string
+ # default = "eu-central-1a"
